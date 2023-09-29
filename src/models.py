50b90d5986be9ae88b8865b499f5f211f23ffbcf
Frossting <109557017+Frossting@users.noreply.github.com>
2023-09-29 20:35:51 +0000

    ready

--- src/models.py
+++ src/models.py
@@ -23,16 +23,38 @@ class Characters(Base):
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    
+    full_name = Column(String(300), nullable=False)
+    eye_color = Column(String(30), nullable=True)
+    hair_color = Column(String(30), nullable=True)                  
 
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Planets(Base): 
+    __tablename__ = "planets"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(180), nullable=False)
+    atmosphere = Column(Boolean, nullable = False)
 
-    def to_dict(self):
-        return {}
+class Vehicles(Base):
+    __tablename__ = "vehicles"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(180), nullable=False)
+    model = Column(String(180), nullable=False)
+    manufacturer = Column(String(300), nullable=False)    
+
+class Favorite(Base):
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key=True)
+
+    User_id = Column(Integer,ForeignKey('user.id'), nullable=False)
+    user=relationship(User)
+
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=True)
+    character = relationship(Characters)
+
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    planet = relationship(Planets)
+
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'), nullable=True)
+    vehicle = relationship(Vehicles)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
